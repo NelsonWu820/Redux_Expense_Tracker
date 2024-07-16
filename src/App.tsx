4798b3d90dc2ccc519c0ce851e3a09f419156c67
Nelson Wu <Nelsonwu820@gmail.com>
2024-07-16 10:21:03 -0400

    added Calendar input field and adapter for days and MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css"
+import Calendar from "./components/Calendar"
+import { LocalizationProvider } from "@mui/x-date-pickers"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Calendar/>
+        </LocalizationProvider>
       </header>
     </div>
   )
