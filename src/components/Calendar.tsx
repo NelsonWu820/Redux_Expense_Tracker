f868b06aea2a07ddb880fac6c2c88152d12b2c5a
Nelson Wu <Nelsonwu820@gmail.com>
2024-07-16 12:36:39 -0400

    added a gloabal date change to when a user choses a new date

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { DatePicker } from '@mui/x-date-pickers'
+import { useAppDispatch } from '../app/hooks';
+import { DatePicker } from '@mui/x-date-pickers';
+import { setDate } from '../app/reducers/date';
+import dayjs from 'dayjs';
 
 const Calendar = () => {
+  const dispatch = useAppDispatch();
+
+  //date is assinged the dayjs object 
+  const newDate = (date: dayjs.Dayjs | null) => {
+    if (date) {
+      dispatch(setDate(date.format('DD-MM-YYYY'))); // Convert Dayjs object to string
+    }
+  }
+
   return (
     <div>
-      <DatePicker />
+      <DatePicker onChange={newDate}/>
     </div>
   )
 }
