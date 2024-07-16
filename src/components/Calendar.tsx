f66636ae51230f6d2ff1e9a17a7c7bf4df49fe6c
Nelson Wu <Nelsonwu820@gmail.com>
2024-07-16 10:20:19 -0400

    created a calnder input field using MUI date picker

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DatePicker } from '@mui/x-date-pickers'
+
+const Calendar = () => {
+  return (
+    <div>
+      <DatePicker />
+    </div>
+  )
+}
+
+export default Calendar
